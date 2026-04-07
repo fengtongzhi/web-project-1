3034ce94ef498a40c0a41beb542e6ead6db83bb8
fengtongzhi <2024218630@mail.hfut.edu.cn>
2026-04-07 22:45:57 +0800

    锁定仓库

--- tlias-web-management/src/main/java/com/feng/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/feng/controller/DeptController.java
@@ -3,11 +3,13 @@ package com.feng.controller;
 import com.feng.pojo.Dept;
 import com.feng.pojo.Result;
 import com.feng.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class DeptController {
 
@@ -20,6 +22,7 @@ public class DeptController {
      */
     @RequestMapping(value = "/depts",method = RequestMethod.GET)//指定请求方式
     public Result list(){
+        log.info("查询所有部门");
         List<Dept> deptList= deptService.findAll();
         return Result.success(deptList);
     }
@@ -29,6 +32,7 @@ public class DeptController {
      */
     @RequestMapping(value = "/depts",method = RequestMethod.DELETE)//指定请求方式
     public Result delete(Integer id){
+        log.info("删除部门，id={}", id);
         deptService.deleteById(id);
         return Result.success();
     }
@@ -40,6 +44,7 @@ public class DeptController {
      */
     @PostMapping("/depts")
     public Result add(@RequestBody Dept dept){
+        log.info("添加部门，dept={}", dept);
         deptService.add(dept);
         return Result.success();
     }
@@ -51,6 +56,7 @@ public class DeptController {
      */
     @GetMapping("/depts/{id}")
     public Result getInfo(@PathVariable("id") Integer id){
+        log.info("根据id查询部门信息，id={}", id);
         Dept dept=deptService.getById(id);
         return Result.success(dept);
     }
@@ -62,6 +68,7 @@ public class DeptController {
      */
     @PutMapping("/depts")
     public Result update(@RequestBody Dept dept){
+        log.info("更新部门信息，dept={}", dept);
         deptService.update(dept);
         return Result.success();
     }
